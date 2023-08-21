a606e050a743ccc90413dcb2a9134d74b21cfda1
Anatolii Ivanov <gotnickname@gmail.com>
2023-08-21 18:03:34 +0600

    added tests for CategoryReposiyory

--- Skillfactory.Module25.EntityFrameworkMSSQL.Tests/CategoryRepositoryTests.cs
+++ Skillfactory.Module25.EntityFrameworkMSSQL.Tests/CategoryRepositoryTests.cs
@@ -32,10 +32,64 @@ namespace Skillfactory.Module25.EntityFrameworkMSSQL.Tests
             repository.CreateCategory(newCategoryName);
             bool actual = repository.DbContext.Categories.Where(c => c.Name == newCategoryName).Any();
 
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetCategoryByIdShouldReturnCorrectObject()
+        {
+            // Arrange.
+            CategoryRepository repository = new CategoryRepository(fixture.DbContext);
+            var expected = repository.DbContext.Categories.OrderBy(c => c.Id).Last();
+
+            // Act.
+            var actual = repository.GetCategoryById(expected.Id);
 
             // Assert.
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void GetAllCategoriesShouldReturnAllCategories()
+        {
+            // Arrange.
+            CategoryRepository repository = new CategoryRepository(fixture.DbContext);
+            var expected = repository.DbContext.Categories.Count();
+
+            // Act.
+            var actual = repository.GetAllCategories().Count;
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void UpdateCategoryNameShouldUpdateDatabase()
+        {
+            // Arrange.
+            CategoryRepository repository = new CategoryRepository(fixture.DbContext);
+            int testObjectId  = repository.DbContext.Categories.OrderBy(c => c.Id).Last().Id;
+            string expected = Guid.NewGuid().ToString();
+
+            // Act.
+            repository.UpdateCategoryName(testObjectId, expected);
+            string actual = repository.DbContext.Categories.SingleOrDefault(c => c.Id == testObjectId).Name;
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void DeleteCategoryShouldDropFromDatabase()
+        {
+            // Arrange.
+            CategoryRepository repository = new CategoryRepository(fixture.DbContext);
+            Category expected  = repository.DbContext.Categories.OrderBy(c => c.Id).Last();
+
+            // Act.
+            repository.DeleteCategory(expected.Id);
+
+            // Assert.
+            Assert.DoesNotContain(expected, repository.DbContext.Categories.ToList());
+        }
     }
 }
