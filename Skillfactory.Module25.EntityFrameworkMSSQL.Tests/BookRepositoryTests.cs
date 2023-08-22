b70cdd92b2c991da73646ad235eda332eefe51c2
Anatolii Ivanov <gotnickname@gmail.com>
2023-08-22 18:29:09 +0600

    added BookRepository with CRUD and tests for it

---
+++ Skillfactory.Module25.EntityFrameworkMSSQL.Tests/BookRepositoryTests.cs
@@ -0,0 +1,117 @@
+﻿using Skillfactory.Module25.EntityFrameworkMSSQL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillfactory.Module25.EntityFrameworkMSSQL.Tests
+{
+    [Collection("Database collection")]
+    public class BookRepositoryTests
+    {
+        /// <summary>
+        /// Class with global db tests setup and context for integration tests.
+        /// </summary>
+        DatabaseFixture fixture;
+
+        public BookRepositoryTests(DatabaseFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void AddBookShouldAddToDatabase()
+        {
+            // Arrange.
+            BookRepository repository = new BookRepository(fixture.DbContext);
+            string testTitle = Guid.NewGuid().ToString();
+            int testYear = 1111;
+            string expected = $"{testTitle} {testYear}";
+
+            // Act.
+            repository.AddBook(testTitle, testYear);
+            string actual = repository.DbContext.Books.Where(b => b.Title == testTitle)
+                .Select(b => $"{b.Title} {b.YearOfPublishing}")
+                .First();
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetBookShouldReturnCorrectObject()
+        {
+            // Arrange.
+            BookRepository repository = new BookRepository(fixture.DbContext);
+            Book expected = repository.DbContext.Books.OrderBy(b => b.Id).First();
+
+            // Act.
+            Book actual = repository.GetBook(expected.Id);
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetAllBooksShouldReturnAllObjects()
+        {
+            // Arrange.
+            BookRepository repository = new BookRepository(fixture.DbContext);
+            List<Book> expected = repository.DbContext.Books.ToList();
+
+            // Act.
+            List<Book> actual = repository.GetAllBooks();
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UpdateBookPublishYearShouldUpdateDatabase()
+        {
+            // Arrange.
+            BookRepository repository = new BookRepository(fixture.DbContext);
+            Book bookToUpdate = repository.DbContext.Books.OrderBy(b => b.YearOfPublishing).First();
+            int expected = 1000;
+
+            // Act.
+            repository.UpdateBookPublishYear(bookToUpdate.Id, expected);
+            int actual = repository.DbContext.Books.Single(b => b.Id == bookToUpdate.Id).YearOfPublishing;
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RemoveAuthorFromBookShouldUpdateDatabase()
+        {
+            // Arrange.
+            BookRepository repository = new BookRepository(fixture.DbContext);
+            Book bookToUpdate = repository.DbContext.Books.OrderByDescending(b => b.Authors.Count).First();
+            Author expected = bookToUpdate.Authors.First();
+
+            // Act.
+            repository.RemoveAuthorFromBook(bookToUpdate.Id, expected.Id);
+            List<Author> actual = repository.DbContext.Books.Single(b => b.Id == bookToUpdate.Id).Authors;
+
+            // Assert.
+            Assert.DoesNotContain(expected, actual);
+        }
+
+        [Fact]
+        public void DeleteBookShouldDropFromDatabase()
+        {
+            // Arrange.
+            BookRepository repository = new BookRepository(fixture.DbContext);
+            Book expected = repository.DbContext.Books.OrderByDescending(b => b.Id).First();
+
+            // Act.
+            repository.DeleteBook(expected.Id);
+            List<Book> actual = repository.DbContext.Books.ToList();
+
+            // Assert.
+            Assert.DoesNotContain(expected, actual);
+        }
+    }
+}
---
+++ Skillfactory.Module25.EntityFrameworkMSSQL/Repositories/BookRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillfactory.Module25.EntityFrameworkMSSQL.Repositories
+{
+    public class BookRepository : BaseRepository
+    {
+        public BookRepository() : base() { }
+        public BookRepository(AppContext appContext) : base(appContext) { }
+
+        public int AddBook(string title, int yearOfPublishing)
+        {
+            DbContext.Books.Add(new Book { Title = title, YearOfPublishing = yearOfPublishing });
+            return DbContext.SaveChanges();
+        }
+
+        public Book GetBook(int bookId)
+        {
+            return DbContext.Books.SingleOrDefault(b => b.Id == bookId);
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return DbContext.Books.ToList();
+        }
+
+        public int UpdateBookPublishYear(int bookId, int bookNewYearOfPublish)
+        {
+            Book bookToUpdate = GetBook(bookId);
+            bookToUpdate.YearOfPublishing = bookNewYearOfPublish;
+            return DbContext.SaveChanges();
+        }
+
+        // Adding book to author credentials realized in AuthorsRepository.
+        public int RemoveAuthorFromBook(int bookId, int authorId)
+        {
+            int result;
+            using (var transactionContext = DbContext.Database.BeginTransaction())
+            {
+                Book bookToUpdate = GetBook(bookId);
+                Author authorToUpdate = bookToUpdate.Authors.SingleOrDefault(a => a.Id == authorId);
+                authorToUpdate.Books.Remove(bookToUpdate);
+                bookToUpdate.Authors.Remove(authorToUpdate);
+                result = DbContext.SaveChanges();
+                transactionContext.Commit();
+            }
+            return result;
+        }
+
+        public int DeleteBook(int bookId)
+        {
+            int result;
+            using (var transactionContext = DbContext.Database.BeginTransaction())
+            {
+                Book bookToDelete = GetBook(bookId);
+                if (bookToDelete.UserId is not null)
+                {
+                    DbContext.Users.SingleOrDefault(u => u.Id == bookToDelete.UserId)
+                        .BooksBorrowed.Remove(bookToDelete);
+                }
+                foreach (Author author in bookToDelete.Authors)
+                {
+                    author.Books.Remove(bookToDelete);
+                }
+                DbContext.Books.Remove(bookToDelete);
+                result = DbContext.SaveChanges();
+                transactionContext.Commit();
+            }
+            return result;
+        }
+    }
+}
